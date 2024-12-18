a3cc8510cd25552efa95b297968cccfe1b40a223
sheldongree <89657544+sheldongree@users.noreply.github.com>
2024-12-18 15:15:21 -0500

    added background sound in game

--- Assets/Scripts/Sound/SoundFXManager.cs
+++ Assets/Scripts/Sound/SoundFXManager.cs
@@ -12,7 +12,7 @@ public class SoundFXManager : MonoBehaviour
             instance = this;
         }
     }
-
+    
     public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume){
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
 
